b30c590d7b23567d5591876c645308bd782005ad
Szakács Márk <szakacsmark@gmail.com>
2021-11-15 10:10:29 +0100

    Modify idle component

--- src/app/idle-screen/idle-screen.component.ts
+++ src/app/idle-screen/idle-screen.component.ts
@@ -8,42 +8,52 @@ declare var $: any;
 })
 export class IdleScreenComponent implements OnInit {
   time = new Date();
+  idleTime: any = 5000;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.setIdleTimeout(5000, function() {
+    this.setIdleTimeout(this.idleTime, function() {
+      // Show modal on inactivity
       $('.inactive-modal').modal('show');
   }, function() {
+    // Hide modal on activity
       $('.inactive-modal').modal('hide');
   });
   
+  // Interval for timer
   window.setInterval( () => {
     this.time = new Date()
   }, 1000);
   }
   
  setIdleTimeout(millis: any, onIdle: any, onUnidle: any) {
+    // Variable for how long is the user inactive
     var timeout: any = 0;
+    // Start the timer
     startTimer();
 
+    // Function for idle timer
     function startTimer() {
         timeout = setTimeout(onExpires, millis);
         document.addEventListener("mousemove", onActivity);
         document.addEventListener("keypress", onActivity);
+        document.addEventListener("click", onActivity);
     }
     
+    // Reset the timer
     function onExpires() {
         timeout = 0;
         onIdle();
     }
 
+    // Function on activities
     function onActivity() {
         if (timeout) clearTimeout(timeout);
         else onUnidle();
-        //since the mouse is moving, we turn off our event hooks for 1 second
         document.removeEventListener("mousemove", onActivity);
         document.removeEventListener("keypress", onActivity);
+        document.addEventListener("click", onActivity);
         setTimeout(startTimer, 1000);
     }
 }
