af105ce3e72672573adfd90bf1cf31abafc203f1
Szakács Márk <szakacsmark@gmail.com>
2021-11-16 13:32:27 +0100

    navbar changes

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
-declare var $: any;
 
 @Component({
   selector: 'app-navigation-bar',
@@ -10,18 +7,9 @@ declare var $: any;
 })
 export class NavigationBarComponent implements OnInit {
   time = new Date();
-  routeChange;
-  constructor(public router: Router) { 
-    this.routeChange = router.events
-      .pipe(
-        filter((event): event is NavigationEnd => event instanceof NavigationEnd)
-      )
-      .subscribe(event => {
-        $('.navbar-toggler').collapse();
-      });
-  }
+  
+  constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
