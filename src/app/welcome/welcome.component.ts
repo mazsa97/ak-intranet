7577c29b8183360c6f1ee37ce8af03e66f3333d0
Szakács Márk <szakacsmark@gmail.com>
2021-12-30 09:02:56 +0100

    Implementet greeting message on welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,9 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  date = new Date();
 
   constructor() { }
 
   ngOnInit(): void { }
+  
+  // Show greeting message based on time of the day
+  showGreeting() {
+    // Get current hour
+    let current = this.date.getHours();
+    let message: string = '';
+    
+    // Messages based on current hour
+    if (current < 9) {
+        message = 'Jó reggelt';
+    }
+    if (current >= 9 && current < 18) {
+        message = 'Jó napot';
+    }
+    if (current >= 18) {
+        message = 'Jó estét';
+    }
+    // Append "," to the end of the text
+    message = message + ',';
+    return message;
+  }
 
 }
