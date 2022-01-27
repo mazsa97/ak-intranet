907d7ca43dc60a441305cb6ad45866b466f2867f
Szakács Márk <szakacsmark@gmail.com>
2022-01-27 13:50:08 +0100

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { WebsitesComponent } from './websites/websites.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { TopButtonComponent } from './top-button/top-button.component';
-import { IdleScreenComponent } from './idle-screen/idle-screen.component';
-import { TelephoneExtensionsComponent } from './telephone-extensions/telephone-extensions.component';
-import { FullCalendarModule } from '@fullcalendar/angular';
-import dayGridPlugin from '@fullcalendar/daygrid';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { CalendarComponent } from './calendar/calendar.component';
-import { FormsModule } from "@angular/forms";
-import { Ng2SearchPipeModule } from "ng2-search-filter";
 import { DesktopComponent } from './navigation-bar/desktop/desktop.component';
-import { MobileComponent } from './navigation-bar/mobile/mobile.component';
+import { FormsModule } from "@angular/forms";
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { HelperComponent } from './navigation-bar/helper/helper.component';
+import { IdleScreenComponent } from './idle-screen/idle-screen.component';
+import { MobileComponent } from './navigation-bar/mobile/mobile.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { Ng2SearchPipeModule } from "ng2-search-filter";
+import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerStatusComponent } from './server-status/server-status.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TelephoneExtensionsComponent } from './telephone-extensions/telephone-extensions.component';
+import { TopButtonComponent } from './top-button/top-button.component';
+import { WebsitesComponent } from './websites/websites.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { environment } from '../environments/environment';
 
 // Register FullCalendar plugins
 FullCalendarModule.registerPlugins([
