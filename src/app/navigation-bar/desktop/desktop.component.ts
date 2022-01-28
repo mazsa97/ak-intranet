67fe5fdc214cbb25a7da2f987793327d11829d80
Szakács Márk <szakacsmark@gmail.com>
2022-01-28 10:31:00 +0100

    Changed desktop component texts

--- src/app/navigation-bar/desktop/desktop.component.ts
+++ src/app/navigation-bar/desktop/desktop.component.ts
@@ -7,6 +7,10 @@ import { NAVIGATIONS } from "../navigations";
   styleUrls: ['./desktop.component.css']
 })
 export class DesktopComponent implements OnInit {
+  logoTitle: string = 'Aranyklinika intranet';
+  logoPath: string = 'assets/images/AK_logo_menu.png';
+  logoAlt: string = 'AK-logo';
+  tooltipHelper: string = 'Súgó';
   navigations = NAVIGATIONS;
 
   constructor() { }
